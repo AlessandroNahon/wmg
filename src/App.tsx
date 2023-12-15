ef44eace8e3ddb03aeaa51c5cc8e50ed7b64c540
alessandronahon <alessandronahon@gmail.com>
2023-12-15 11:08:44 -0500

    feature: todo list!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type List = {
+	id: number
+	title: string
 }
 
-export default App;
+function App() {
+	const [todos, setTodos] = useState<List[]>([{ id: 1, title: 'Groceries' }])
+	const [complete, setComplete] = useState<List[] | []>([])
+
+	function handlePrompt(todos: List[]) {
+		const title = window.prompt('Enter your new task')
+
+		if (title === '' || !title) {
+			return
+		}
+
+		setTodos([...todos, { id: todos.length + 1, title }])
+	}
+
+	function handleOnComplete(
+		todo: List,
+		{ todos, complete }: { todos: List[]; complete: List[] }
+	) {
+		const itemExists = complete.some((c) => c.id === todo.id)
+
+		if (!itemExists) setComplete([...complete, todo])
+
+		const updatedTodos = todos.filter((t) => t.id !== todo.id)
+		setTodos(updatedTodos)
+	}
+
+	return (
+		<div className='App'>
+			<button onClick={() => handlePrompt(todos)}>Add Todo</button>
+			<div>
+				<ListComponent
+					headerText='Todo'
+					list={todos}
+					actionName='✅'
+					handleOnComplete={handleOnComplete}
+					todos={todos}
+					complete={complete}
+				/>
+			</div>
+
+			<div>
+				<ListComponent headerText='Completed' list={complete} />
+			</div>
+		</div>
+	)
+}
+
+export default App
+
+type Props = {
+	headerText: string
+	list: List[]
+	actionName?: string
+	handleOnComplete?: (
+		li: List,
+		{ todos, complete }: { todos: List[]; complete: List[] }
+	) => void
+	todos?: List[] | []
+	complete?: List[] | []
+}
+
+function ListComponent({
+	headerText,
+	list,
+	actionName,
+	todos,
+	complete,
+	handleOnComplete,
+}: Props) {
+	return (
+		<div>
+			<h2>{headerText}</h2>
+			<ul>
+				{list
+					.map((li) => (
+						<li>
+							{handleOnComplete && actionName && todos && complete && (
+								<button
+									onClick={() => handleOnComplete(li, { todos, complete })}
+								>
+									{actionName}
+								</button>
+							)}
+							{li.title}
+						</li>
+					))
+					.reverse()}
+			</ul>
+		</div>
+	)
+}
